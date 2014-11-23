16491117b28e17b4730b52890da29970763a515c
Michael Luskind <michaelluskind@Michaels-MacBook-Pro.local>
2014-11-23 16:51:28 -0500

    updated

--- app/router.js
+++ app/router.js
@@ -11,7 +11,7 @@ Router.map(function() {
     this.route('new');
   });
   this.resource('post', { path: 'posts/:id' }, function(){
-  	this.route('edit');
+  	this.route('edit', { path: 'edit'} );
   });
 });
 
